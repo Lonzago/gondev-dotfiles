038c7957b174d5730ac465de5bc6e9cf988c13f6
GonzaloBLab <gonzalo.vicente@baboonlab.com>
2026-04-29 23:40:36 +0100

    Add initial configuration files for zsh and installation script for Arch Linux

--- scripts/install-hypr-arch.sh
+++ scripts/install-hypr-arch.sh
@@ -17,13 +17,21 @@ sudo pacman -S hyprland rofi waybar fastfetch gcc git curl thunar eza dosfstools
 #Install Terminal Stuffs
 sudo pacman -S neovim kitty zsh ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick yazi
 
+#Install Browser
+sudo pacman -S firefox
+
 # Install yay 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si 
+
+
+
 
 # Install AUR packages
 yay -S swww waypaper
 # Install oh-my-zsh and plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo -e "\n\tCambiando la shell basica por zsh"
+chsh -s $(which zsh)
