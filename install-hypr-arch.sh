c0da7f2da5fde8d9fe569cfc46dfaed47f21eef6
GonzaloBLab <gonzalo.vicente@baboonlab.com>
2026-04-29 23:33:02 +0100

    Initial commit

---
+++ install-hypr-arch.sh
@@ -0,0 +1,29 @@
+#! bin/bash
+
+
+echo -e "\n\t=== === === === === === === === === === ==="
+echo -e "\t===   DotFiles Gondev 0.01 Arch Linux   ==="
+echo -e "\t=== === === === === === === === === === ===\n\n"
+
+
+
+echo -e "\n\t Installing dependencies"
+
+sudo pacman -Syu
+
+# Install basics
+sudo pacman -S hyprland rofi waybar fastfetch gcc git curl thunar eza dosfstools ntfsprogs nwg-look
+
+#Install Terminal Stuffs
+sudo pacman -S neovim kitty zsh ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick yazi
+
+# Install yay 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si 
+
+# Install AUR packages
+yay -S swww waypaper
+# Install oh-my-zsh and plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
