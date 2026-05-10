6de76b82065bb35ea671bcaf8cb1fada3763f82f
GonDVRDev <gonzalodvrdev@gmail.com>
2026-05-10 16:28:42 +0100

    update neo-tree behaviour

--- nvim/.config/nvim/lua/plugins/neo-tree-options.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree-options.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  lazy = false,
   opts = {
     filesystem = {
       filtered_items = {
@@ -8,6 +9,21 @@ return {
         hide_gitignored = false,
       },
     },
-    enable_preview = true,
+    window = {
+      neo_tree_preview = true,
+      mappings = {
+        ["p"] = {
+          "toggle_preview",
+          config = {
+            use_float = false,
+            use_snacks_image = true,
+            use_image_nvim = true,
+          },
+        },
+        -- ["l"] = "focus_preview",
+        ["<C-b>"] = { "scroll_preview", config = { direction = 10 } },
+        ["<C-f>"] = { "scroll_preview", config = { direction = -10 } },
+      },
+    },
   },
 }
