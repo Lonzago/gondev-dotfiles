45d07393f7124ba7340f69a68e3eeb9bf1b9be88
GonDVRDev <gonzalodvrdev@gmail.com>
2026-05-18 01:47:01 +0100

    install: enhance arch setup script with oh-my-zsh and plugins
    - Added automatic Oh My Zsh installation if not present
    - Added installation of external zsh plugins:
      - zsh-autosuggestions (Fish-like command suggestions)
      - zsh-syntax-highlighting (real-time syntax coloring)
    - Added bat package to terminal tools for better file viewing
    - Updated comments and output messages for better UX
    - Added next steps instructions after installation completes

--- scripts/install-hypr-arch.sh
+++ scripts/install-hypr-arch.sh
@@ -16,19 +16,43 @@ sudo pacman -Syu
 sudo pacman -S hyprland rofi waybar fastfetch gcc git curl thunar eza dosfstools ntfsprogs nwg-look
 
 #Install Terminal Stuffs
-sudo pacman -S neovim kitty zsh ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick yazi
+sudo pacman -S neovim kitty zsh ffmpeg 7zip jq poppler fd ripgrep fzf zoxide imagemagick yazi bat
 
 #Install Browser
 sudo pacman -S firefox
 
-# Install yay 
+# Install yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si 
-
+makepkg -si
 
 
 
 # Install AUR packages
 yay -S swww waypaper
+
+# Install Oh My Zsh (if not already installed)
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+    echo -e "\n\t Installing Oh My Zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --skip-chsh
+fi
+
+# Install Oh My Zsh external plugins
+echo -e "\n\t Installing Oh My Zsh plugins"
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom/plugins"
+
+# zsh-autosuggestions
+if [ ! -d "$ZSH_CUSTOM/zsh-autosuggestions" ]; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/zsh-autosuggestions"
+fi
+
+# zsh-syntax-highlighting
+if [ ! -d "$ZSH_CUSTOM/zsh-syntax-highlighting" ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/zsh-syntax-highlighting"
+fi
+
+echo -e "\n\t === Installation complete! ==="
+echo -e "\n\t Next steps:"
+echo -e "\t 1. Restart your terminal or run: exec zsh"
+echo -e "\t 2. Deploy your dotfiles: cd gondev-dotfiles && ./scripts/stow-deploy.sh"
 
